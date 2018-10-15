c9838048a7ec644231aed1d7e655043887b61f99
Bryonna Baines <bbaines09@gmail.com>
2018-10-15 11:52:58 -0700

    added function to populate the toolbar with the breed name in detail view

---
+++ app/src/main/java/com/bryonnabaines/dogbreedlist/views/DogDetailActivity.kt
@@ -0,0 +1,42 @@
+package com.bryonnabaines.dogbreedlist.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import com.bryonnabaines.dogbreedlist.R
+import com.squareup.picasso.Picasso
+
+class DogDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_detail)
+
+        val imageView : ImageView = findViewById(R.id.imageView)
+        val photo = intent.getSerializableExtra(DOG) as String
+
+        //Picasso will load in into the imageView
+        photo.let {
+            Picasso.with(this).load(it)
+                    .into(imageView)
+            supportActionBar?.title = parseBreedFromUrl(it)
+        }
+
+        imageView.setOnClickListener{
+            finish()
+        }
+    }
+
+    private fun parseBreedFromUrl( url: String) : String {
+
+        val shortUrl = url.removeRange(0,30)
+        Log.d("DOG DETAIL", shortUrl)
+
+        return shortUrl.removeRange(shortUrl.indexOf('/'), shortUrl.length)
+    }
+
+    companion object {
+        val DOG = "DOG"
+    }
+}
